cdbab7e7c3cb0995a7f075ed108d63b31a10ce90
Opal-Skies <micah@koolwaaij.nl>
2026-02-22 12:15:10 +0100

    player changes

--- CharacterBody2d.cs
+++ CharacterBody2d.cs
@@ -2,36 +2,55 @@ using Godot;
 
 public partial class CharacterBody2d : CharacterBody2D
 {
-	public const float Speed = 50.0f;
-	public const float JumpVelocity = -40.0f;
-
+	private const float MaxSpeed = 384.0f;
+	private const float JumpVelocity = -512.0f;
+	
+	//State machine: 0=ground, 1=air,
+	public int State;
+	
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
-
+		if (State == 0)
+		{
+			MoveHorizontal(1.0f,256.0f);
+			GroundedJump();
+		}else if (State == 1)
+		{
+			MoveHorizontal(0.2f,64.0f);
+		}
+		
 		// Add the gravity.
 		if (!IsOnFloor())
 		{
 			velocity += GetGravity() * (float)delta;
 		}
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_up") && IsOnFloor())
+		void GroundedJump()
 		{
-			velocity.Y = JumpVelocity;
+			if (Input.IsActionJustPressed("ui_up"))
+			{
+				velocity.Y = JumpVelocity;
+				State = 1;
+			}
 		}
+		
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		if (direction != Vector2.Zero)
+		void MoveHorizontal(float Acceleration, float Friction)
 		{
-			velocity.X = direction.X * Speed;
+			Vector2 direction = Input.GetVector("ui_left", "ui_right", "0", "0");
+			if (direction != Vector2.Zero)
+			{
+				velocity.X += ((MaxSpeed*direction.X - velocity.X)/8)*Acceleration;
+			}
+			else
+			{
+				velocity.X = Mathf.MoveToward(Velocity.X, 0, Friction);
+			}
 		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-		}
+		
 
 		Velocity = velocity;
 		MoveAndSlide();
